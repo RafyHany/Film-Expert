f32c30810f8371ce70f0aa1c1e9df2cdb9f422b7
Armia Joseph <armia.joseph35@gmail.com>
2024-05-16 18:07:10 +0300

    facts added

---
+++ dataset.pl
@@ -0,0 +1,45 @@
+%comedy%
+comedy("Ace Ventura: Pet Detective",["Jim Carrey", "Courteney Cox", "Sean Young", "Tone Loc", "Dan Marino"], 1994).
+comedy("The Grand Budapest Hotel",["Ralph Fiennes", "Tony Revolori", "Saoirse Ronan", "Adrien Brody", "Willem Dafoe"],2014).
+comedy("Crazy Rich Asians",["Constance Wu", "Henry Golding", "Awkwafina", "Michelle Yeoh", "Gemma Chan"],2018).
+comedy("Caddyshack",["Chevy Chase", "Rodney Dangerfield", "Bill Murray", "Ted Knight", "Michael O'Keefe"],1980).
+comedy("Ghostbusters",["Bill Murray", "Dan Aykroyd", "Sigourney Weaver","Harold Ramis", "Rick Moranis"],1984).
+comedy("Ferris Bueller's Day Off",["Matthew Broderick", "Alan Ruck", "Mia Sara", "Jeffrey Jones","Jennifer Grey"] ,1986).
+comedy("Coming to America",["Eddie Murphy", "Arsenio Hall", "James Earl Jones", "John Amos", "Madge Sinclair"],1988).
+comedy("The Breakfast Club",["Emilio Estevez", "Anthony Michael Hall", "Judd Nelson", "Molly Ringwald", "Ally Sheedy"],1985).
+
+%superheros%
+superheros("Thor: Love and Thunder",["Chris Hemsworth","Natalie Portman","Tessa Thompson","Christian Bale","Taika Waititi"],2022).
+superheros("Spider-Man: No Way Home",["Tom Holland","Zendaya","Benedict Cumberbatch","Jacob Batalon","Willem Dafoe"],2021).
+superheros("Avengers: Endgame",["Robert Downey Jr.","Chris Evans","Tom Holland","Chris Hemsworth","Mark Ruffalo"],2019).
+superheros("Guardians of the Galaxy",["Chris Pratt","Zoe Saldana","Dave Bautista","Vin Diesel","Bradley Cooper"],2014).
+superheros("Doctor Strange",["Benedict Cumberbatch","Chiwetel Ejiofor","Rachel McAdams","Benedict Wong","Mads Mikkelsen"],2016).
+
+%cartoon%
+cartoon("Kung Fu Panda",["Jack Black","Dustin Hoffman","Angelina Jolie","Ian McShane","Jackie Chan"],2008).
+cartoon("The Incredibles",["Craig T. Nelson","Holly Hunter","Samuel L. Jackson","Jason Lee","Sarah Vowell"],2004).
+cartoon("Megamind",["Will Ferrell","Tina Fey","Jonah Hill","David Cross","Brad Pitt"],2010).
+cartoon("Big Hero 6",["Ryan Potter","Scott Adsit","Jamie Chung","Damon Wayans Jr.","Genesis Rodriguez"],2014).
+cartoon("Wreck-It Ralph",["John C. Reilly","Sarah Silverman","Jack McBrayer","Jane Lynch","Alan Tudyk"],2012).
+
+%science_fiction%
+science_fiction("Blade Runner 2049", ["Ryan Gosling", "Harrison Ford", "Ana de Armas", "Sylvia Hoeks", "Jared Leto"], 2017).
+science_fiction("Interstellar",["Matthew McConaughey","Anne Hathaway"," Jessica Chastain", "Michael Caine", "Casey Affleck"] , 2014).
+science_fiction("Inception",["Leonardo DiCaprio","Joseph Gordon-Levitt","Ellen Page","Tom Hardy", " Ken Watana"], 2010).
+science_fiction("Dune",["Timothée Chalamet","Rebecca Ferguson","Oscar Isaac", "Josh Brolin"], 2021).
+science_fiction("The Terminator", ["Arnold Schwarzenegger", "Michael Biehn", "Linda Hamilton", "Paul Winfield", "Lance Henriksen"], 1984).
+science_fiction("Avatar", ["Sam Worthington", "Zoe Saldana", "Sigourney Weaver", "Stephen Lang", "Michelle Rodriguez"], 2009).
+science_fiction("The Matrix", ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving", "Joe Pantoliano"], 1999).
+
+%Action%
+action("John Wick", [ "Keanu Reeves", "Ian McShane", "Willem Dafoe", "John Leguizamo","Lance Reddick"] ,2014).
+action("Mad Max: Fury Road",[ "Tom Hardy","Charlize Theron","Nicholas Hoult","Hugh Keays,Byrne","Rosie Huntington","Whiteley"],2015).
+action("The Raid: Redemption",[ "Iko Uwais", "Yayan Ruhian", "Donny Alamsyah", "Joe Taslim", "Pierre Gruno"], 2011).
+action("Die Hard", ["Bruce Willis", "Alan Rickman", "Bonnie Bedelia", "Reginald VelJohnson", "Alexander Godunov"], 1988).
+action("The Dark Knight", ["Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine", "Maggie Gyllenhaal"], 2008).
+action("Mission: Impossible - Fallout", ["Tom Cruise", "Henry Cavill", "Ving Rhames", "Simon Pegg", "Rebecca Ferguson"], 2018).
+
+%Drama:*
+drama("The Shawshank Redemption" ,["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler","Clancy Brown"], 1994).
+drama("The Pursuit of Happyness",["Will Smith", "Jaden Smith", "Thandie Newton", "Brian Howe", "James Karen"],2006).
+drama("12 Years a Slave",[ "Chiwetel Ejiofor", "Michael Fassbender", "Lupita Nyongo", "Sarah Paulson", "Benedict Cumberbatch"], 2013).
